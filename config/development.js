033b3902e0c79b1c44d89f75d7959a6e7d9972fe
irfan Hasbek <irfanhasbek@gmail.com>
2015-06-01 13:58:27 +0300

    config update

--- config/development.js
+++ config/development.js
@@ -2,6 +2,6 @@ module.exports = {
     dbpath : "mongodb://localhost/alimsatim",
     port : 7000,
     loglevel : 'debug',
-    //host : "http://178.62.134.23:7000"
-    host : "http://localhost:7000"
+    host : "http://178.62.134.23:7000"
+    //host : "http://localhost:7000"
 };
